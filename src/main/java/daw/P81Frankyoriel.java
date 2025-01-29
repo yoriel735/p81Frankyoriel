1f84b20d3be76c866eb53356c8d5efaa48546598
yoriel <yoriellorenzo345@gmail.com>
2025-01-29 13:45:55 +0100

    Primer commit: Archivos base del proyecto

---
+++ src/main/java/daw/P81Frankyoriel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package daw;
+
+/**
+ *
+ * @author yoriel
+ */
+public class P81Frankyoriel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
