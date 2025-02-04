030ec4e25e00f7917d71805e51e738399ace5c83
yoriel <yoriellorenzo345@gmail.com>
2025-02-04 09:30:46 +0100

    commit 4 creo

--- src/main/java/Modelos/IVeterinario.java
+++ src/main/java/Modelos/IVeterinario.java
@@ -3,32 +3,32 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Modelos;
+import java.sql.SQLException;
 import java.util.List;
 /**
  *
  * @author yoriel
  */
-public class IVeterinario {
+public interface IVeterinario {
 
   // Obtener todos los registros de la tabla veterinario
-    List<VeterinarioDTO> getAll() throws SQLException;
+    List<VeterinariosDTO> getAll() throws SQLException;
 
     // Obtener un veterinario por su clave primaria (id)
-    VeterinarioDTO findByPk(int pk) throws SQLException;
+    VeterinariosDTO findByPk(int pk) throws SQLException;
 
     // Insertar un nuevo veterinario en la base de datos
-    int insertVeterinario(VeterinarioDTO veterinario) throws SQLException;
+    int insertVeterinario(VeterinariosDTO veterinario) throws SQLException;
 
     // Insertar varios veterinarios a la vez
-    int insertVeterinario(List<VeterinarioDTO> lista) throws SQLException;
+    int insertVeterinario(List<VeterinariosDTO> lista) throws SQLException;
 
     // Eliminar un veterinario de la base de datos
-    int deleteVeterinario(VeterinarioDTO veterinario) throws SQLException;
+    int deleteVeterinario(VeterinariosDTO veterinario) throws SQLException;
 
     // Eliminar todos los veterinarios de la tabla
     int deleteVeterinarios() throws SQLException;
 
     // Actualizar un veterinario, dado su id (pk) y los nuevos datos
-    int updateVeterinario(int pk, VeterinarioDTO nuevosDatos) throws SQLException;
-}
+    int updateVeterinario(int pk, VeterinariosDTO nuevosDatos) throws SQLException;
 }
--- src/main/java/Modelos/Imascota.java
+++ src/main/java/Modelos/Imascota.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Modelos;
+import java.sql.SQLException;
 import java.util.List;
 /**
  *
